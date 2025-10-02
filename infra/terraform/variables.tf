92f06328d38181442222b060a7a8f1aa21015dc4
sqauproba <3687563+sqauproba@users.noreply.github.com>
2025-10-02 09:39:00 +0700

    feat(infra): implement variables

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "project" {
+  description = "Cloud project identifier"
+  type        = string
+}
+
+variable "region" {
+  description = "Deployment region"
+  type        = string
+  default     = "us-central1"
+}
